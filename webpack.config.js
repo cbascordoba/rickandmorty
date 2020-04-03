7730395dab83f087a03bd1b522ca91b805b00596
cbascordoba14 <cbascordoba14@gmail.com>
2020-04-03 00:41:24 -0500

    Dependencias y creacion del template para el home

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// traer path de node para acceder donde nos estamos moviendo dentro del proyecto 
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// vamos a crear un modulo que vamos a exportar
+module.exports = {
+    entry: './src/index.js',
+    // vamos a indicar el output donde va a ir nuestro proyecto ya compilado
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        // el compilado de nuestro js se va a llamar main.js
+        filename: 'main.js'
+    },
+    resolve:{
+        extensions:['.js'],
+
+    },
+    module:{
+        rules:[
+            {
+                // rejects valores que queremos filtrar de una ruta, elementos o archivos.
+                test:  /\.js?$/,
+                // ahora vamos a excluir la carpeta node modules a lo que seria mi proyecto 
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                }
+        }
+    ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin([
+            {
+                inject: true,
+                template: './public/index.html',
+                filename: './index.html'
+            }
+        ])
+    ]
+}
